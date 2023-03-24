34d372c90f5bff2c7b99ae1daccef87e98a4b691
Daryl White <53910321+djwfyi@users.noreply.github.com>
2023-03-24 14:59:13 -0500

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,24 @@
+import { clipboard } from "./clipboard";
+import { header } from "./header";
+import { imagePreview } from "./imagePreview";
+import { siteSearch } from "./search";
+import { sidebar } from "./sidebar";
+import { toc } from "./toc";
+
+// Search
+siteSearch();
+
+// Header
+header();
+
+// Sidebar
+sidebar();
+
+// TOC
+toc();
+
+// Image preview
+imagePreview();
+
+// Clipboard
+clipboard();
